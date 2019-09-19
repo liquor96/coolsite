fa170968bc826ecda5f03513c31459200537c2ee
liquor96 <hyh_1996@126.com>
2019-09-19 18:51:33 +0800

    init search

---
+++ src/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="searchDiv">
+    <a-input-search 
+    placeholder="输入搜索内容" 
+    style="width: 400px;height: 50px" 
+    @search="onSearch" 
+    class="searchInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "search",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scope>
+.searchDiv {
+    position: absolute;
+    height: 50%;
+    left:0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+}
+</style>
