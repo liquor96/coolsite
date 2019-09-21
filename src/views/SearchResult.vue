c375c46718432cbc5bc9cf7161e5a619ce8d9d0a
liquor96 <hyh_1996@126.com>
2019-09-22 01:00:16 +0800

    init

--- src/views/SearchResult.vue
+++ src/views/SearchResult.vue
@@ -1,28 +1,38 @@
 
 <template>
   <a-tabs @change="callback" type="card">
-    <a-tab-pane tab="百度" key="1">{{this.$route.query.q}}</a-tab-pane>
-    <a-tab-pane tab="谷歌" key="2">
-        <iframe src="https://www.baidu.com/s?wd=你好" frameborder="0" width="100%" height="600px"></iframe>
+    <a-tab-pane tab="百度" key="1">
+      <iframe :src="baiduUrl" frameborder="0" class="iframecotainer"></iframe>
     </a-tab-pane>
-    <a-tab-pane tab="必应" key="3">Content of Tab Pane 3</a-tab-pane>
+    <a-tab-pane tab="谷歌" key="2">
+      <iframe :src="googleUrl" frameborder="0" class="iframecotainer"></iframe>
+    </a-tab-pane>
+    <a-tab-pane tab="必应" key="3">
+      <iframe :src="bingUrl" frameborder="0" class="iframecotainer"></iframe>
+    </a-tab-pane>
   </a-tabs>
 </template>
 <script>
 export default {
-  name: 'SearchResult',
-  data () {
+  name: "SearchResult",
+  data() {
     return {
-    }
+      baiduUrl: "https://www.baidu.com/s?wd=" + this.$route.query.q,
+      bingUrl: "https://cn.bing.com/search?q=" + this.$route.query.q,
+      googleUrl: "https://g.lijianm.in/search?q=" + this.$route.query.q,
+    };
   },
   methods: {
-    callback (key) {
-      console.log(key)
-    },
-  },
-}
+    callback(key) {
+      console.log(key);
+    }
+  }
+};
 </script>
 
 <style scope>
-
+.iframecotainer {
+  width: 100%;
+  height: calc(90vh);
+}
 </style>
